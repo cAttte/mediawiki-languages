005dc38359c84849839cb69eea1d29de461beb28
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-16 14:22:54 -0300

    feat(fetch): generate and store typings

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -3,22 +3,29 @@
  * It is not part of the exposed interface, and depends on dev-dependencies.
  */
 const fs = require("fs/promises")
-const rimraf = require("rimraf")
 const { promisify } = require("util")
+const rimraf = promisify(require("rimraf"))
 const { Octokit } = require("@octokit/rest")
 const fetchList = require("./fetchList")
 const fetchBlob = require("./fetchBlob")
 const extractName = require("./extractName")
+const generateTypings = require("./generateTypings")
 const deserialize = { php: require("./deserialize/php") }
 
 const repo = { owner: "wikimedia", repo: "mediawiki" }
+const remk = async dir => {
+    await rimraf(`${__dirname}/${dir}`)
+    await fs.mkdir(`${__dirname}/${dir}`)
+}
 
 async function main() {
-    await promisify(rimraf)(`${__dirname}/../data/`)
-    await fs.mkdir(`${__dirname}/../data/`)
+    await remk("../data")
+    await remk("../typings")
 
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
     const list = await fetchList(octokit, repo)
+    /** @type {typeof list} */
+    const englishData = {}
 
     for (const set of Object.keys(list)) await fs.mkdir(`${__dirname}/../data/${set}`)
     for (const [set, blobs] of Object.entries(list)) {
@@ -30,7 +37,16 @@ async function main() {
             const language = extractName(blob.name)
             await fs.writeFile(`${__dirname}/../data/${set}/${language}.json`, json)
             console.log(`Fetched ${set}/${language}.json.`)
+            if (language === "en") englishData[set] = data
         }
+    }
+
+    for (const [set, data] of Object.entries(englishData)) {
+        const interface = set[0].toUpperCase() + set.slice(1)
+        const typings = generateTypings(data, interface)
+        const code = `export default ${typings}\n`
+        await fs.writeFile(`${__dirname}/../typings/${interface}.d.ts`, code)
+        console.log(`Generated typings for ${interface}.d.ts.`)
     }
 }
 
