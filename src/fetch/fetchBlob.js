4bf2dc57de1f4c323ccf687cc38ca3aeda8bcc3d
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-16 02:04:34 -0300

    fix(fetchBlob): decode base64

--- src/fetch/fetchBlob.js
+++ src/fetch/fetchBlob.js
@@ -7,5 +7,7 @@ const { Octokit } = require("@octokit/rest")
  */
 module.exports = async function fetchBlob(octokit, repo, hash) {
     const blobResponse = await octokit.git.getBlob({ ...repo, file_sha: hash })
-    return blobResponse.data.content
+    const encoded = blobResponse.data.content
+    const content = Buffer.from(encoded, "base64").toString("utf-8")
+    return content
 }
