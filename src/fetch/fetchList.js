1f43b392341053fadfdad8b7227116a2e312328c
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-16 01:49:23 -0300

    refactor(fetchList): return blob hashes too

--- src/fetch/fetchList.js
+++ src/fetch/fetchList.js
@@ -2,10 +2,9 @@ const { Octokit } = require("@octokit/rest")
 
 /**
  * @param {Octokit} octokit
+ * @param {{ owner: string, repo: string }} repo
  */
-module.exports = async function fetchList(octokit) {
-    const repo = { owner: "wikimedia", repo: "mediawiki" }
-
+module.exports = async function fetchList(octokit, repo) {
     const commitResponse = await octokit.repos.listCommits({ per_page: 1, ...repo })
     const commit = commitResponse.data[0].commit
     const hash = commit.tree.sha
@@ -17,7 +16,8 @@ module.exports = async function fetchList(octokit) {
     const langTreeResponse = await octokit.git.getTree({ ...repo, tree_sha: lang.sha })
     const langTree = langTreeResponse.data.tree
 
-    /** @type {{ messages: string[] }} */
+    /** @typedef {{ name: string, hash: string }[]} BlobList */
+    /** @type {{ messages: BlobList }} */
     const output = { messages: null /*, data: null, i18n: null */ }
 
     for (const setName of ["messages" /*, "data", "i18n" */]) {
@@ -25,7 +25,7 @@ module.exports = async function fetchList(octokit) {
         const setTreeResponse = await octokit.git.getTree({ ...repo, tree_sha: set.sha })
         const setTree = setTreeResponse.data.tree
         const blobs = setTree.filter(item => item.type === "blob")
-        output[setName] = blobs.map(blob => blob.path)
+        output[setName] = blobs.map(blob => ({ name: blob.path, hash: blob.sha }))
     }
 
     return output
