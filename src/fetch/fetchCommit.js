d11f598e98b43a2e7d66714239e7d290a28ab957
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-16 17:41:04 -0300

    feat(fetch): create fetchCommit()

---
+++ src/fetch/fetchCommit.js
@@ -0,0 +1,11 @@
+const { Octokit } = require("@octokit/rest")
+
+/**
+ * @param {Octokit} octokit
+ * @param {{ owner: string, repo: string }} repo
+ */
+module.exports = async function fetchList(octokit, repo) {
+    const commitResponse = await octokit.repos.listCommits({ per_page: 1, ...repo })
+    const commit = commitResponse.data[0].commit
+    return commit
+}
